f3a2d120f059a26d9ff71e184d1b3eabebe1685f
anmorales-hub <moralesand2018@gmail.com>
2021-02-15 17:01:30 -0500

    Fix: update coveralls

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     verbose: true,
     testURL: "http://localhost:8000/",
-    "coverageDirectory": "./Tests/coverage",
+    "coverageDirectory": "./tests/coverage",
 
     "coveragePathIgnorePatterns": [
         "./jest",
